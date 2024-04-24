75af966ecedcce6b83cc36b37f0dbcb26ab5d3cf
volemy <minayobenevolence0@gamil.com>
2024-04-25 02:31:03 +0300

    Task 4

--- 0x01-ES6_promise/4-user-promise.js
+++ 0x01-ES6_promise/4-user-promise.js
@@ -1,7 +1,8 @@
 export default function signUpUser(firstName, lastName) {
   return new Promise((resolve) => {
-    setTimeout(() => {
-      resolve({ firstName, lastName });
-    }, 1000);
+    resolve({
+      firstName,
+      lastName,
+    });
   });
 }
