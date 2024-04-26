cd6dd4bed72e757a2666dcc7a3eb4f74791ae01f
volemy <minayobenevolence0@gmail.com>
2024-04-26 06:41:31 +0000

    Drectory deleted

--- 0x00-ES6_basic/10-loops.js
+++
@@ -1,7 +0,0 @@
-export default function appendToEachArrayValue(array, appendString) {
-  for (const value of array) {
-    array[array.indexOf(value)] = appendString + value;
-  }
-
-  return array;
-}
