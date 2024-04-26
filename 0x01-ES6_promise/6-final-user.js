0e3ace5e8ff8ad62da97c7cef1e641e8b44aa39f
volemy <minayobenevolence0@gmail.com>
2024-04-26 06:18:12 +0000

    Handling multiple promises

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,3 +1,23 @@
-export default function loadBalancer(chinaDownload, USDownload) {
-  return Promise.race([chinaDownload, USDownload]);
-}
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
+
+export default (firstName, lastName, fileName) => {
+  const promises = [signUpUser(firstName, lastName),
+    uploadPhoto(fileName)];
+
+  return Promise.allSettled(promises)
+    .then((values) => {
+      const res = [];
+      values.forEach((value) => {
+        if (value.status === 'fulfilled') {
+          res.push(value);
+        } else {
+          res.push({
+            status: value.status,
+            value: `Error: ${value.reason.message}`,
+          });
+        }
+      });
+      return res;
+    });
+};
