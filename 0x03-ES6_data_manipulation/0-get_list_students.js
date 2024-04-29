d0f77de89d5cacd749fc59f661d1a573d07e70d3
volemy <minayobenevolence0@gamil.com>
2024-04-29 22:34:27 +0300

    task 0

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,7 @@
+const getListStudents = () => [
+  { id: 1, firstname: 'Guillaume', lastname: 'San Francisco' },
+  { id: 2, firstname: 'James', lastname: 'Columbia' },
+  { id: 5, firstname: 'Serena', lastname: 'San Francisco' },
+];
+
+export default getListStudents;
