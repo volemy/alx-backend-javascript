b98f5ae4694dada575e33e8d41dbe52692888102
volemy <minayobenevolence0@gamil.com>
2024-05-01 23:23:49 +0300

    task 3

---
+++ 0x04-TypeScript/task_3/js/main.ts
@@ -0,0 +1,10 @@
+import { RowID, RowElement } from './interface';
+import * as CRUD from './crud';
+
+const row: RowElement = { firstName: 'Guillaume', lastName: 'Salva' };
+const newRowID: RowID = CRUD.insertRow(row);
+
+const updatedRow: RowElement = { firstName: 'Guillaume', lastName: 'Salva', age: 23 };
+CRUD.updateRow(newRowID, updatedRow);
+
+CRUD.deleteRow(newRowID);
